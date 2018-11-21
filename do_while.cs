f1e405c5033d29d024b6d26127478d474dd2effe
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2018-11-21 18:38:55 +0200

    Add files via upload

---
+++ do_while.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_while
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = 1;
+			int i = 0;
+			Double sum = 0;
+			do
+			{
+				Console.WriteLine("enter a number:");
+				number = Convert.ToInt32(Console.ReadLine());
+				i = i+ 1;
+				sum = sum + number;
+				
+			}
+
+			while (number != 0);
+			i = i - 1;
+			Console.WriteLine(sum/i);
+		}
+	}
+}
